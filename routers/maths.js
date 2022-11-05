3c709e75e4a0808e77303e17b49d874162ede298
diegoSilbestro <diegosilbestro@gmail.com>
2022-11-05 09:59:20 -0300

    c

--- routers/maths.js
+++ routers/maths.js
@@ -4,17 +4,8 @@ const {divisionController} = require("../controllers/divisionController")
 const {additionController} = require("../controllers/additionController")
 
 
-//routerMaths.get ('/:dividendo/:divisor', divisionController)
+routerMaths.get ('/:dividendo/:divisor', divisionController)
 
-routerMaths.get ('/:a/:b', (res,req) =>{
-    
-    let num1 = Number(req.params.a),
-        num2 = Number (req.params.b);
-    if (num1 < 0 || num2 < 0) {
-        res.send ('No se deben enviar valores menores a 0')
-    } else {
-        res.json( (num1+num2))
-    }
-})
+routerMaths.get ('/:a/:b', additionController)
 
 module.exports = routerMaths;
